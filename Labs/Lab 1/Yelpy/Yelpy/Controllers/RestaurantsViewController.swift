558c30a3707320a8d0753102781ced4aa8693565
Myat Thu Ko <nymyatthuko@gmail.com>
2021-02-05 20:01:56 -0500

    Milestone 3: Build the basic Restaurants Feed Completed

--- Labs/Lab 1/Yelpy/Yelpy/Controllers/RestaurantsViewController.swift
+++ Labs/Lab 1/Yelpy/Yelpy/Controllers/RestaurantsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AlamofireImage
 
-class RestaurantsViewController: UIViewController {
+class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // ––––– TODO: Add storyboard Items (i.e. tableView + Cell + configurations for Cell + cell outlets)
     // ––––– TODO: Next, place TableView outlet here
@@ -17,11 +17,16 @@ class RestaurantsViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     // –––––– TODO: Initialize restaurantsArray
-    var restaurantsArray = [[String:Any?]] = []
+    var restaurantsArray: [[String:Any?]] = []
 
     // ––––– TODO: Add tableView datasource + delegate
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getAPIData()
     }
     // ––––– TODO: Get data from API helper and retrieve restaurants
     func getAPIData() {
@@ -31,7 +36,37 @@ class RestaurantsViewController: UIViewController {
             }
             print(restaurants)
             self.restaurantsArray = restaurants
+            self.tableView.reloadData() // reload data!
         }
+    }
+    
+    // Protocol Stubs
+    
+    // numberOfRowInSection tells the table view how many rows, or cells in this case, to create.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantsArray.count
+    }
+    
+    // What type of cell are we configuring
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create Restaurant Cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell") as! RestaurantCell
+        
+        let restaurant = restaurantsArray[indexPath.row]
+        
+        // Set Label to the restaurant name for each cell
+        cell.label.text = restaurant["name"] as? String ?? ""
+        
+        // Set Image of restaurant
+        // 1. Get the image url string from the restaurant dictionary
+        if let imageURLString = restaurant["image_url"] as? String {
+            // 2. Get the convert url string -> url
+            let imageURL = URL(string: imageURLString)
+            // 3. Set image using the image url with AlamofireImage
+            cell.restaurantImage.af.setImage(withURL: imageURL!)
+        }
+        
+        return cell
     }
 }
 
