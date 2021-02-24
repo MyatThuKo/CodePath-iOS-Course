1b71ef6de3d8f7582eb13b42443a13d4d5dd3f2e
Myat Thu Ko <nymyatthuko@gmail.com>
2021-02-23 21:25:57 -0500

    Configuring Detail View Completed

--- Assignments/Assignment_1/Flix/Flix/MovieDetailsViewController.swift
+++ Assignments/Assignment_1/Flix/Flix/MovieDetailsViewController.swift
@@ -5,17 +5,33 @@
 //  Created by Myat Thu Ko on 2/23/21.
 //
 
+import AlamofireImage
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet var backdropView: UIImageView!
+    @IBOutlet var posterView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var synopsisLabel: UILabel!
     
     var movie: [String: Any]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        print(movie["title"])
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        posterView.af_setImage(withURL: posterURL!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        backdropView.af.setImage(withURL: backdropURL!)
     }
     
 
