62022f299dc9fb083fb1928a6d9cb12f2624fa98
Myat Thu Ko <nymyatthuko@gmail.com>
2021-03-11 18:44:28 -0500

    Added character count label and functionality to track the count

--- Assignments/Assignment_2/Twitter/Twitter/ViewControllers/TweetViewController.swift
+++ Assignments/Assignment_2/Twitter/Twitter/ViewControllers/TweetViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
     @IBOutlet var tweetTextView: UITextView!
+    @IBOutlet var characterCountLabel: UILabel!
+    
+    let maxLength = 280
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
+        characterCountLabel.text = "0/280"
+    }
+    
+    @objc func textViewDidChange(_ textView: UITextView) {
+        characterCountLabel.text = "\(tweetTextView.text.count)/\(maxLength)"
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentText = tweetTextView.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+        return updatedText.count <= maxLength
     }
     
     @IBAction func cancelButton(_ sender: Any) {
@@ -34,13 +49,13 @@ class TweetViewController: UIViewController {
         }
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
